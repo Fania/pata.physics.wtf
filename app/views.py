fb549788e5cf118c07c2933d11cf20c83945eeff
Fania Ra <fania@web.de>
2014-07-09 20:27:27 +0100

    proto01 sort of working

--- app/views.py
+++ app/views.py
@@ -28,79 +28,65 @@ def p01results():
     # query = request.args.get('query', '')
     # print 'query: ', query
 
+    # data = {
+    #     'query': request.form['query']
+    #     'pre_sens':
+    #     'post_sens':
+    # }
+
+    query = request.form['query']
+
     if request.method == 'GET':
-        q = request.form['query']
-        print 'p01results get: ', q
+        print 'p01results get: ', query  # data['query']
         # return render_template('p01results.html', q)
     else:
         #request was a POST
-        q = request.form['query']
-        print 'p01results post: ', q
-        return render_template('p01results.html', query=q)
+        print 'p01results post: ', query  # data['query']
+        # qx = getResults(q)
 
-    # error = False
-    # if query:
-    #     if not query:
-    #         error = True
-    #     else:
+       # CLINAMEN
+        sens = dict([])
+        pre_sens = dict([])
+        post_sens = dict([])
+        clinamen_words = clinamen(query, 2)
+        clinamen_len = len(clinamen_words)
+        for r in clinamen_words:
+            if len(pre_sentence(r)) > 0:
+                pre_sens[r] = pre_sentence(r)
+            if len(post_sentence(r)) > 0:
+                post_sens[r] = post_sentence(r)
+            if len(find_sentence(r)) > 0:
+                sens[r] = find_sentence(r)
+        # SYZYGY
+        syssens = dict([])
+        pre_syssens = dict([])
+        post_syssens = dict([])
+        syzygy_words = syzygy(query)
+        syzygy_len = len(syzygy_words)
+        for r in syzygy_words:
+            if len(pre_sentence(r)) > 0:
+                pre_syssens[r] = pre_sentence(r)
+            if len(post_sentence(r)) > 0:
+                post_syssens[r] = post_sentence(r)
+            if len(find_sentence(r)) > 0:
+                syssens[r] = find_sentence(r)
+        # ANTINOMY
+        antisens = dict([])
+        pre_antisens = dict([])
+        post_antisens = dict([])
+        antinomy_words = antinomy(query)
+        antinomy_len = len(antinomy_words)
+        for r in antinomy_words:
+            if len(pre_sentence(r)) > 0:
+                pre_antisens[r] = pre_sentence(r)
+            if len(post_sentence(r)) > 0:
+                post_antisens[r] = post_sentence(r)
+            if len(find_sentence(r)) > 0:
+                antisens[r] = find_sentence(r)
+                #print antisens[r]
 
-    #         # SESSIONS
-    #         # pop(key), keys(), items(), get(key), clear(), flush()
-    #         # s = request.session
-    #         # s.set_expiry(120)
-    #         # if 'queries' in s.keys():
-    #         #     s['queries'].append(query)
-    #         #     #if len(s['queries']) > 9:
-    #         #     #    s['queries'].pop([0])
-    #         # else:
-    #         #     s['queries'] = [query]
-    #         # s.save()
-    #         # print s.session_key, s.get_expiry_age(), s.get_expire_at_browser_close()
-    #         # print 's:', s.items()
-
-    #         # CLINAMEN
-    #         sens = dict([])
-    #         pre_sens = dict([])
-    #         post_sens = dict([])
-    #         clinamen_words = clinamen(query, 2)
-    #         clinamen_len = len(clinamen_words)
-    #         for r in clinamen_words:
-    #             if len(pre_sentence(r)) > 0:
-    #                 pre_sens[r] = pre_sentence(r)
-    #             if len(post_sentence(r)) > 0:
-    #                 post_sens[r] = post_sentence(r)
-    #             if len(find_sentence(r)) > 0:
-    #                 sens[r] = find_sentence(r)
-    #         # SYZYGY
-    #         syssens = dict([])
-    #         pre_syssens = dict([])
-    #         post_syssens = dict([])
-    #         syzygy_words = syzygy(query)
-    #         syzygy_len = len(syzygy_words)
-    #         for r in syzygy_words:
-    #             if len(pre_sentence(r)) > 0:
-    #                 pre_syssens[r] = pre_sentence(r)
-    #             if len(post_sentence(r)) > 0:
-    #                 post_syssens[r] = post_sentence(r)
-    #             if len(find_sentence(r)) > 0:
-    #                 syssens[r] = find_sentence(r)
-    #         # ANTINOMY
-    #         antisens = dict([])
-    #         pre_antisens = dict([])
-    #         post_antisens = dict([])
-    #         antinomy_words = antinomy(query)
-    #         antinomy_len = len(antinomy_words)
-    #         for r in antinomy_words:
-    #             if len(pre_sentence(r)) > 0:
-    #                 pre_antisens[r] = pre_sentence(r)
-    #             if len(post_sentence(r)) > 0:
-    #                 post_antisens[r] = post_sentence(r)
-    #             if len(find_sentence(r)) > 0:
-    #                 antisens[r] = find_sentence(r)
-    #                 #print antisens[r]
-
-    #         return render_template('p01results.html')
-    # return render_template('p01results.html', error=error)
+        #print data
+        return render_template('p01results.html', **locals())
 
 
 @app.route('/p02')
